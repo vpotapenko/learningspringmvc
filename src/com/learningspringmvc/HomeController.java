feb0e57ac90a5d6f9337063441e541528fac950b
vpotapenko <vyacheslav_potapenko@epam.com>
2013-07-12 17:49:45 +0300

    updates

--- src/com/learningspringmvc/HomeController.java
+++ src/com/learningspringmvc/HomeController.java
@@ -12,9 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
     @RequestMapping(value = "/")
     public String home() {
         System.out.println("HomeController: Passing through...");
         return "WEB-INF/views/home.jsp";
     }
+
 }
