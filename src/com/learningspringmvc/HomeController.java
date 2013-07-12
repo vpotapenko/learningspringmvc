d6a9edc2ec38db7b711b037458a6896b3c0c4126
vpotapenko <vyacheslav_potapenko@epam.com>
2013-07-12 17:30:01 +0300

    added Controller

---
+++ src/com/learningspringmvc/HomeController.java
@@ -0,0 +1,20 @@
+package com.learningspringmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vyacheslav_Potapenko
+ * Date: 7/12/13
+ * Time: 5:04 PM
+ */
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/")
+    public String home() {
+        System.out.println("HomeController: Passing through...");
+        return "WEB-INF/views/home.jsp";
+    }
+}
